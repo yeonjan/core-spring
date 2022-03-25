2a685e158da9c03bf4dc5ae2bb634468a755277e
Yeonjin Jeong <y_jin99@naver.com>
2022-03-25 16:06:10 +0900

    ComponentScan

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,17 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@ComponentScan(
+        basePackages =  "hello.core.member", // 지정하지 않으면  hello.core부터 다
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+
+
+
+}
